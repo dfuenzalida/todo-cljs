854f3b59d09829e99eabefa02348c5d3d67ca0ed
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2013-02-24 15:58:54 -0300

    get-uuid and window.onload

--- src/cljs/todo_cljs/todos.cljs
+++ src/cljs/todo_cljs/todos.cljs
@@ -2,7 +2,18 @@
 
 ;; Simple test
 (defn hello[]
-  (js/alert "Hello!"))
+  (js/alert "hello!"))
 
-(defn ^:export init []
-  (hello))
+;; This get-uuid fn is almost equiv to the original
+(defn get-uuid []
+  (apply
+   str
+   (map
+    (fn [x]
+      (if (= x \0)
+        (.toString (bit-or (* 16 (.random js/Math)) 0) 16)
+        x))
+    "00000000-0000-4000-0000-000000000000")))
+
+;; window.onload ...
+(.addEventListener js/window "load" hello false)
