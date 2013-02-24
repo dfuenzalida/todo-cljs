abd5a19c26cba34b31dcb847382cbba54d9f9c2e
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2013-02-24 16:24:25 -0300

    handlers, listeners, formatting

--- src/cljs/todo_cljs/todos.cljs
+++ src/cljs/todo_cljs/todos.cljs
@@ -1,8 +1,17 @@
 (ns todo-cljs.todos)
 
-;; Simple test
-(defn hello[]
-  (js/alert "hello!"))
+;; helpers, constants...
+(def ENTER_KEY 13)
+(defn by-id [id] (.getElementById js/document id))
+(defn hello[] (js/alert "hello!"))
+
+(defn add-todo [text]
+  (let [trimmed (.trim text)]
+    (js/alert trimmed)))
+
+(defn new-todo-handler [ev]
+  (if (= ENTER_KEY (.-keyCode ev))
+    (add-todo (.-value (by-id "new-todo")))))
 
 ;; This get-uuid fn is almost equiv to the original
 (defn get-uuid []
@@ -15,5 +24,12 @@
         x))
     "00000000-0000-4000-0000-000000000000")))
 
+
+(defn add-event-listeners []
+  (.addEventListener (by-id "new-todo") "keypress" new-todo-handler false))
+
+(defn window-load-handler []
+  (add-event-listeners))
+
 ;; window.onload ...
-(.addEventListener js/window "load" hello false)
+(.addEventListener js/window "load" window-load-handler false)
